427ced67668dcc7245dc34c1bf98c38cd405a1a5
Alexandros Papakostopoulos <a.papakostopoulos@gmail.com>
2023-09-24 12:42:08 +0300

    regions_get.py

---
+++ ookla/united_kingdom/regions_get.py
@@ -0,0 +1,75 @@
+import pandas as pd
+from helium import *
+from bs4 import BeautifulSoup
+
+url = "https://www.speedtest.net/performance/united-kingdom"
+
+browser = start_firefox(url, headless=True)
+
+soup = BeautifulSoup(browser.page_source, "html.parser")
+
+countries = []
+
+for li in soup.find_all("li"):
+    if "/performance/" in li.a.get("href"):
+        print("https://www.speedtest.net" + li.a.get("href"))
+        countries.append("https://www.speedtest.net" + li.a.get("href"))
+
+browser.quit()
+
+print(len(countries))
+
+for entry in countries:
+    if entry.endswith("united-states"):
+        countries.remove(entry)
+
+strings = [
+    "/de/",
+    "/es",
+    "/ja/",
+    "/nl/",
+    "/pt/",
+    "/th/",
+    "zh-Hans",
+    "zh-Hant",
+    "/ar/",
+    "/fr/",
+    "/id/",
+    "/it/",
+    "/ko/",
+    "/pl/",
+    "/sv/",
+]
+
+for country in countries:
+    for string in strings:
+        if string in country:
+            countries.remove(country)
+
+for entry in countries:
+    if entry.endswith("united-kingdom"):
+        countries.remove(entry)
+
+for entry in countries:
+    if entry.endswith("united-kingdom"):
+        countries.remove(entry)
+
+for entry in countries:
+    if entry.endswith("united-kingdom"):
+        countries.remove(entry)
+
+for entry in countries:
+    if entry.endswith("zh-Hans/performance/united-kingdom"):
+        countries.remove(entry)
+
+print(countries)
+print(len(countries))
+
+# df = pd.DataFrame(countries)
+
+# df.to_csv(
+#     "C:/Python/web_scraping_2/web_scraping/ookla/united_states/states_get.csv",
+#     index=None,
+#     sep="|",
+#     header=False,
+# )
